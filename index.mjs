0883367357377e37f040e74c603ca794fb01c3f3
Dimitris Grammatikogiannis <grammatikogiannis.d@wideservices.gr>
2025-07-28 13:41:20 +0300

    init

---
+++ index.mjs
@@ -0,0 +1,31 @@
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import { cwd } from "process";
+
+// Get the version from the version.php file
+const regex = /\$plugin->version\s*=\s*(\d+);/g;
+
+if (!existsSync(cwd() + "/version.php")) {
+  console.log("version.php not found");
+  exit(1);
+}
+
+const file = readFileSync(cwd() + "/version.php", "utf8");
+const vm = RegExp(regex).exec(file)
+
+if (vm) {
+  const version = vm[1];
+  const currentDate = new Date();
+  let versionId = Number.parseInt(version.substring(8), 10);
+
+  const versionYear = currentDate.getFullYear();
+  let versionMonth = currentDate.getMonth() + 1;
+  versionMonth = versionMonth.toString().padStart(2, "0");
+  let versionDay = currentDate.getDate();
+  versionDay = versionDay.toString().padStart(2, "0");
+  versionId = versionId + 1;
+  versionId = versionId.toString().padStart(3, "0");
+
+  const newVersion = `${versionYear}${versionMonth}${versionDay}${versionId}`;
+
+  writeFileSync(cwd() + "/version.php", file.replace(version, newVersion));
+}
---
+++ version.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Version information for local_training
+ *
+ * @package    local_training
+ * @copyright  2025 Digitech
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_training';
+$plugin->version   = 20250728008;
+$plugin->requires  = 2022041900; // Moodle 4.0 or later.
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = 'v0.1';
